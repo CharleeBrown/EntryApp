7fbe9d2f8a1daa2d01dcb36fb55c0fb8ba56280a
Coleman <charleebrown22@gmail.com>
2022-08-30 16:01:01 -0500

    Added chakra

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import * as React from "react";
 import {ChakraProvider} from '@chakra-ui/react';
 import { Box, Center, Image, Flex, Badge, Text } from "@chakra-ui/react";
-import { MdStar } from "react-icons/md"
+
 
 function App() {
   return (
