947bf23f69ad906eabbfedde2ca8fbf499e155a1
Coleman <charleebrown22@gmail.com>
2022-08-30 16:04:54 -0500

    added Chakra parts

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import * as React from "react";
 import {ChakraProvider} from '@chakra-ui/react';
-import { Box, Center, Image, Flex, Badge, Text } from "@chakra-ui/react";
+import { Input, Text } from "@chakra-ui/react";
 
 
 function App() {
@@ -10,10 +10,8 @@ function App() {
     <ChakraProvider>
     <div className="App">
       <header className="App-header">
-        <p>
-          Enter Option
-        </p>
-        <Text />
+        <Text>Enter Option</Text>
+        <Input placeholder="Enter Info" />
       </header>
     </div>
     </ChakraProvider>
