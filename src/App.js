c19bbf656597a769142ba07a5ac0869288adb6f9
Coleman <charleebrown22@gmail.com>
2022-08-31 08:47:11 -0500

    Added enter function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import * as React from "react";
 import {ChakraProvider} from '@chakra-ui/react';
 import { Input, Text, Box } from "@chakra-ui/react";
-
+import {useState} from "react";
 
 function App() {
+  function handleKey(event){
+    if(event.keyCode ===13){
+      alert("test");
+    }
+  }
   return (
     <ChakraProvider>
     <div className="App">
       <header className="App-header">
         <Box>
-        <Text>Enter Option</Text>
-        <Input placeholder="Enter Info" />
+        <Text></Text>
+        <Input placeholder="Enter Info" onKeyDown={handleKey}/>
         </Box>
       </header>
     </div>
