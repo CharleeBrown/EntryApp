706516e890b41577a3b9f6f968ab77ec76672f4e
Coleman <charleebrown22@gmail.com>
2022-08-31 08:17:44 -0500

    Added a box

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import * as React from "react";
 import {ChakraProvider} from '@chakra-ui/react';
-import { Input, Text } from "@chakra-ui/react";
+import { Input, Text, Box } from "@chakra-ui/react";
 
 
 function App() {
@@ -10,8 +10,10 @@ function App() {
     <ChakraProvider>
     <div className="App">
       <header className="App-header">
+        <Box>
         <Text>Enter Option</Text>
         <Input placeholder="Enter Info" />
+        </Box>
       </header>
     </div>
     </ChakraProvider>
