ab75dc21ec14e0e4f7862f8928549163b8cfa489
Coleman <charleebrown22@gmail.com>
2022-09-01 08:48:23 -0500

    removing routing for now

--- src/App.js
+++ src/App.js
@@ -11,15 +11,6 @@ function App() {
   return (
     <ChakraProvider>
     <div className="App">
-    <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/expenses">Expenses</Link>
-      </nav>
       <header className="App-header">
         <DataGrab></DataGrab>
       </header>
