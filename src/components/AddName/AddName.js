c7b11f5982eb0c22bd92b56644b9e8856eb64fb4
Coleman <charleebrown22@gmail.com>
2022-09-01 11:35:10 -0500

    sending headers

--- src/components/AddName/AddName.js
+++ src/components/AddName/AddName.js
@@ -2,6 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './AddName.module.css';
 import {ChakraProvider, Input, Text, Box, Button} from '@chakra-ui/react';
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from '@chakra-ui/react'
 import {useState} from 'react';
 function AddName(name){ 
     let url = "https://mainsendapi.herokuapp.com/";
@@ -12,15 +18,18 @@ function AddName(name){
       fetch(url,
         { method:'POST',
           mode:'cors', 
+          headers:new Headers({"Content-type":"application/json"}),
       body:JSON.stringify({names})})
       console.log(JSON.stringify(names))
     }
   return (
     <ChakraProvider>
   <Box className={styles.AddName}>
-    <Text>Enter a name</Text>
-    <Input placeholder="Enter a name"  onChange={e=> setName(e.target.value)}></Input>
-    <Button colorScheme="blue" onClick={sendName}>Add New Name</Button>
+    <FormControl>
+      <FormLabel>Enter a Name</FormLabel>
+      <Input placeholder="Enter a name"  onChange={e=> setName(e.target.value)}></Input>
+      <Button colorScheme="blue" onClick={sendName}>Add New Name</Button>
+    </FormControl>
   </Box>
   </ChakraProvider>
 );
