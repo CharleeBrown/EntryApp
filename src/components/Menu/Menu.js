a8c38e8eb41fe1a882476fff1a4fd0dce9301933
Coleman <charleebrown22@gmail.com>
2022-09-12 10:49:49 -0500

    Adding routing

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Menu.module.css';
 
+import * as React from "react";
+import {ChakraProvider} from '@chakra-ui/react';
+import { Link } from "react-router-dom";
+import {
+  Routes,
+  Route, 
+  Outlet
+}  from "react-router-dom";
+import './Menu.css';
 const Menu = () => (
-  <div className={styles.Menu}>
-    Menu Component
+  <ChakraProvider>
+  <div className="menuMain">
+
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Outlet/>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      
+      </div>
   </div>
+  </ChakraProvider>
 );
+function Home() {
+  return ;
+}
+function About() {
+  return;
+}
 
-Menu.propTypes = {};
 
-Menu.defaultProps = {};
 
-export default Menu;
+export default  Menu;
