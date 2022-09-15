ffd9c7ccaf11258cc62bbd83d5ed6ffbc61e96f2
Coleman <charleebrown22@gmail.com>
2022-09-15 12:01:30 -0500

    Fixing the linking

--- src/App.js
+++ src/App.js
@@ -13,9 +13,7 @@ function App() {
   return (
     <ChakraProvider>
     <div className="App">
-      
-      <header className="App-header">  
-   
+      <header className="App-header">
         <Box p='10' border="solid 1px white" borderRadius="10px">
           <DataGrab></DataGrab>
           </Box>
@@ -23,14 +21,8 @@ function App() {
         <AddName></AddName>
         </Box>
       </header>
-      <footer>  <noscript>You need to enable JavaScript to run this app.</noscript>
-<a title="Google Analytics Alternative" href="https://clicky.com/101380580"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
-<script async src="//static.getclicky.com/101380580.js"></script>
-<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101380580ns.gif" /></p></noscript></footer>
     </div>
-    
     </ChakraProvider>
-    
   );
 
 }
